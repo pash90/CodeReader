3d09f44412db83f3531653c03a44d9aaf3c23fbf
Parth Mehta <parth@bluechilli.com>
2017-05-16 10:29:30 +1000

    Initial commit.
    Setup the basic structure of the project

--- Code Reader/HistoryTableViewController.swift
+++ Code Reader/HistoryTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class HistoryTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
--- Code Reader/PassViewController.swift
+++ Code Reader/PassViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class PassViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
